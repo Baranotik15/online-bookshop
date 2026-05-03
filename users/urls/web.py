eabffa9267dd5f1f48c7615447b182cdb4b8a4d4
TopalRuslan <baranotik15@gmail.com>
2026-05-03 07:39:21 +0300

    Add email confirmation on registration

--- users/urls/web.py
+++ users/urls/web.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from users.views.web import register, profile, edit_profile
+from users.views.web import register, profile, edit_profile, confirm_email
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('confirm-email/<str:token>/', confirm_email, name='confirm-email'),
     path('profile/', profile, name='profile'),
     path('profile/edit/', edit_profile, name='edit-profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),

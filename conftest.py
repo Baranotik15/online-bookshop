64178750ec64e29987d5012a8640916fc73f3afb
TopalRuslan <baranotik15@gmail.com>
2026-05-11 11:12:31 +0300

    feat: add Celery for async email sending on registration

--- conftest.py
+++ conftest.py
@@ -18,3 +18,9 @@ def use_local_cache(settings):
             'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         }
     }
+
+
+@pytest.fixture(autouse=True)
+def celery_eager(settings):
+    settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.CELERY_TASK_EAGER_PROPAGATES = True
